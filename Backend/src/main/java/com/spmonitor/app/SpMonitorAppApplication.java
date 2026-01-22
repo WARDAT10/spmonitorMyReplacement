48ddcd9829dadb2782c2d8b55c134b8717c8b1d0
Ramadhani-Yassin <yasynramah@gmail.com>
2026-01-22 09:55:37 +0300

    Initial commit: SP Monitor  With Backend Files Postgres

---
+++ Backend/src/main/java/com/spmonitor/app/SpMonitorAppApplication.java
@@ -0,0 +1,13 @@
+package com.spmonitor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpMonitorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpMonitorAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/spmonitor/app/SpMonitorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spmonitor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpMonitorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
